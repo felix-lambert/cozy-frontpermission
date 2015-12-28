12beb3cbd358b12aa7e0e352f5027a084e6d6901
lemelon <lambertfelix8@gmail.com>
2015-12-28 13:12:23 +0100

    add scope

--- app/scripts/bookmarks/Home.Ctrl.js
+++ app/scripts/bookmarks/Home.Ctrl.js
@@ -1,8 +1,8 @@
 angular.module('Bookmarks').controller('HomeAngCtrl', HomeAngCtrl);
 
-HomeAngCtrl.$inject = ['$scope', '$injector'];
+HomeAngCtrl.$inject = ['$scope', '$injector', '$rootScope'];
 
-function HomeAngCtrl($scope, $injector) {
+function HomeAngCtrl($scope, $injector, $rootScope) {
 
 	var vm        = this;
 	vm.getContact = getContact;
--- scripts/app.js
+++ scripts/app.js
@@ -23,9 +23,9 @@ function appConfig($httpProvider, $routeProvider) {
 }
 ;angular.module('Bookmarks').controller('HomeAngCtrl', HomeAngCtrl);
 
-HomeAngCtrl.$inject = ['$scope', '$injector'];
+HomeAngCtrl.$inject = ['$scope', '$injector', '$rootScope'];
 
-function HomeAngCtrl($scope, $injector) {
+function HomeAngCtrl($scope, $injector, $rootScope) {
 
 	var vm        = this;
 	vm.getContact = getContact;
