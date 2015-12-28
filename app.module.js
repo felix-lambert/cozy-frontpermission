d1495f6c8bc4751bb383fec83bd584c2d37bd818
lemelon <lambertfelix8@gmail.com>
2015-12-28 14:36:22 +0100

    add scope

--- app.module.js
+++ app.module.js
@@ -15,6 +15,7 @@ appConfig.$inject = ['$httpProvider', '$routeProvider'];
 
 function appConfig($httpProvider, $routeProvider) {
   for (var path in routeObject) {
+    console.log(path);
     $routeProvider.when(path, routeObject[path]);
   }
   // $routeProvider.otherwise({
