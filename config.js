9184f356cb0e535de77fb5a16f821a8fe00394e0
lemelon <lambertfelix8@gmail.com>
2015-12-30 09:59:41 +0100

    test

--- config.js
+++ config.js
@@ -14,7 +14,9 @@ exports.config = {
                 "before": [
                   'vendor/scripts/angular.js',
                   'vendor/scripts/angular-resource.js',
-                  'vendor/scripts/angular-route.js'
+                  'vendor/scripts/angular-route.js',
+                  'vendor/scripts/require.js',
+                  'vendor/scripts/domReady.js'
                 ]
             }
         },
@@ -28,24 +30,5 @@ exports.config = {
     },
     "paths": {
         "public": "."
-    },
-    "plugins": {
-        "browsefiry": {
-            "bundles": {
-                'scripts/app.js': {
-                    'entry': "app/app.module.js",
-                    'matcher': '/^app/',
-                    onBrowserifyLoad: function(bundler) {
-                        console.log('onWatchifyLoad');
-                    },
-                    onBeforeBundle: function(bundler) {
-                        console.log('onBeforeBundle');
-                    },
-                    onAfterBundle: function(error, bundleContents) {
-                        console.log('onAfterBundle');
-                    }
-                }
-            }
-        }
     }
 };
