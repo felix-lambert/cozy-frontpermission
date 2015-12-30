a6e953975feb86202dc5073f143bc2a12cc0e460
lemelon <lambertfelix8@gmail.com>
2015-12-30 07:43:50 +0100

    test

--- scripts/app.js
+++ scripts/app.js
@@ -18,11 +18,14 @@ function appConfig($httpProvider, $routeProvider) {
     console.log(path);
     $routeProvider.when(path, routeObject[path]);
   }
-  // $routeProvider.otherwise({
-  //   redirectTo: '/'
-  // });
+  $routeProvider.otherwise({
+    redirectTo: '/'
+  });
 }
-;angular.module('Bookmarks').controller('HomeAngCtrl', HomeAngCtrl);
+;var cozydb = require('cozydb-browser');
+
+angular.module('Bookmarks').controller('HomeAngCtrl', HomeAngCtrl);
+
 
 HomeAngCtrl.$inject = ['$scope', '$injector', '$rootScope'];
 
@@ -32,41 +35,9 @@ function HomeAngCtrl($scope, $injector, $rootScope) {
 	vm.getContact = getContact;
 
   	function getContact() {
-  		console.log('Get contact');
-  		window.parent.postMessage({
-  			action: 'getToken',
-            name: "frontpermission"
-  		}, '*');
-
-        window.addEventListener("message", function(event) {
-        	console.log(event);
-        	intent = event.data;
-            if (intent.token) {
-                location = window.location;
-                url = location.protocol + "//" + location.host + "/ds-api/request/contact/all/";
-                xhr = new XMLHttpRequest();
-                xhr.open('POST', url, true);
-                xhr.onload = function() {
-                    $rootScope.contacts = xhr.response;
-                    $rootScope.$apply();
-                }
-                xhr.onerror = function(e) {
-                    err = "Request failed : #{e.target.status}";
-                    console.log(err);
-                }
-                xhr.setRequestHeader('Content-Type', 'application/json');
-                token = btoa("frontpermission:" + intent.token);
-                authorization = "Basic " + token;
-                xhr.setRequestHeader("Authorization", authorization);
-                xhr.send();
-            } else {
-                console.log("Weird intent, cannot handle it", intent);
-                window.onerror("Error handling intent: " + intent, "MainRouter.initialize", null, null,
-                    new Error()
-                );
-            }
-        }, true);
-  	}
+  		console.log('_____________Get contact_____________');
+        cozydb.escape();
+    }
 }
 ;
 //# sourceMappingURL=app.js.map
