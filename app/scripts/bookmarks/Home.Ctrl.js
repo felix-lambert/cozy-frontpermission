ee1a5626b4eb4c3ece0151c64a5ba3336f0a426c
lemelon <lambertfelix8@gmail.com>
2016-01-05 18:40:42 +0100

    bonjour

--- app/scripts/bookmarks/Home.Ctrl.js
+++ app/scripts/bookmarks/Home.Ctrl.js
@@ -6,10 +6,12 @@ function HomeAngCtrl($scope, $injector, $rootScope) {
 
 	var vm        = this;
 	vm.getContact = getContact;
+	var data = {};
 
   	function getContact() {
     	console.log('_____________Get contact_____________');
-        Cozy.alert();
+        data = Cozy.getData('ds-api', 'request', 'contact');
+        console.log(data);
         console.log('End get contact');
     }
 }
--- vendor/scripts/cozydb-browser.js
+++ vendor/scripts/cozydb-browser.js
@@ -4,44 +4,61 @@
   'use strict'
   function defineCozy() {
     var Cozy = {};
+    var url = String;
+    var request = String;
     Cozy.alert = function() {
       alert("This is a test message from the cozy framework");
     };
+
+    Cozy.checkExists = function(id) {
+      
+      url = location.protocol + "//" + location.host + "/data/exist/" + id;
+      request = 'GET';
+      window.parent.postMessage({action: 'getToken'}, '*');
+    };
+
+    Cozy.getData = function(accessType, type, appName) {
+      url = location.protocol + "//" + location.host + "/" + accessType "/" + type + "/" + appName + "/all/";
+      request = 'POST'
+      window.parent.postMessage({action: 'getToken'}, '*');
+    };
+
+      window.addEventListener("message", function(event) {
+        intent = event.data;
+        if (intent.token) {
+          location = window.location;
+          
+          xhr = new XMLHttpRequest();
+          xhr.open(request, url, true);
+          xhr.onload = function() {
+              console.log(xhr.response);
+              return (xhr.response);
+              // $rootScope.contacts = xhr.response;
+              // $rootScope.$apply();
+          }
+          xhr.onerror = function(e) {
+              err = "Request failed : #{e.target.status}";
+              console.log(err);
+              }
+              xhr.setRequestHeader('Content-Type', 'application/json');
+              token = btoa(intent.appName + ":" + intent.token);
+              authorization = "Basic " + token;
+              xhr.setRequestHeader("Authorization", authorization);
+              xhr.send();
+          } else {
+              console.log("Weird intent, cannot handle it", intent);
+              window.onerror("Error handling intent: " + intent, "MainRouter.initialize", null, null,
+                  new Error()
+              );
+          }
+      }, true);
+    };
+
     return Cozy;
   }
 
   if (typeof(Cozy) === 'undefined') {
     window.Cozy = defineCozy();
   }
-
-  window.addEventListener("message", function(event) {
-    intent = event.data;
-    if (intent.token) {
-      location = window.location;
-      url = location.protocol + "//" + location.host + "/ds-api/request/contact/all/";
-      xhr = new XMLHttpRequest();
-      xhr.open('POST', url, true);
-      xhr.onload = function() {
-          console.log(xhr.response);
-          // $rootScope.contacts = xhr.response;
-          // $rootScope.$apply();
-      }
-      xhr.onerror = function(e) {
-          err = "Request failed : #{e.target.status}";
-          console.log(err);
-          }
-          xhr.setRequestHeader('Content-Type', 'application/json');
-          token = btoa("frontpermission:" + intent.token);
-          authorization = "Basic " + token;
-          xhr.setRequestHeader("Authorization", authorization);
-          xhr.send();
-      } else {
-          console.log("Weird intent, cannot handle it", intent);
-          window.onerror("Error handling intent: " + intent, "MainRouter.initialize", null, null,
-              new Error()
-          );
-      }
-  }, true);
 })(window);
 
-window.parent.postMessage({action: 'getToken'}, '*');
