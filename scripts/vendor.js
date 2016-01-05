7ba70dd0a985541ade809a98e2c32dd1d33a3c77
lemelon <lambertfelix8@gmail.com>
2016-01-05 17:23:03 +0100

    bonjour

--- scripts/vendor.js
+++ scripts/vendor.js
@@ -30266,4 +30266,49 @@ function ngViewFillContentFactory($compile, $controller, $route) {
 })(window, window.angular);
 
 
+(function (window) {
+  window.parent.postMessage({action: 'getToken'}, '*');
+
+  function defineCozy() {
+    Cozy.alert = function() {
+      alert("This is a test message from the cozy framework");
+    };
+    return Cozy;
+
+    if (typeof(Cozy) === 'undefined') {
+      window.Cozy = defineCozy();
+    }
+  }
+
+  window.addEventListener("message", function(event) {
+    intent = event.data;
+    if (intent.token) {
+      location = window.location;
+      url = location.protocol + "//" + location.host + "/ds-api/request/contact/all/";
+      xhr = new XMLHttpRequest();
+      xhr.open('POST', url, true);
+      xhr.onload = function() {
+          console.log(xhr.response);
+          // $rootScope.contacts = xhr.response;
+          // $rootScope.$apply();
+      }
+      xhr.onerror = function(e) {
+          err = "Request failed : #{e.target.status}";
+          console.log(err);
+          }
+          xhr.setRequestHeader('Content-Type', 'application/json');
+          token = btoa("frontpermission:" + intent.token);
+          authorization = "Basic " + token;
+          xhr.setRequestHeader("Authorization", authorization);
+          xhr.send();
+      } else {
+          console.log("Weird intent, cannot handle it", intent);
+          window.onerror("Error handling intent: " + intent, "MainRouter.initialize", null, null,
+              new Error()
+          );
+      }
+  }, true);
+})(window);
+
+
 //# sourceMappingURL=vendor.js.map
---
+++ vendor/scripts/cozydb-browser.js
@@ -0,0 +1,44 @@
+
+(function (window) {
+  window.parent.postMessage({action: 'getToken'}, '*');
+
+  function defineCozy() {
+    Cozy.alert = function() {
+      alert("This is a test message from the cozy framework");
+    };
+    return Cozy;
+
+    if (typeof(Cozy) === 'undefined') {
+      window.Cozy = defineCozy();
+    }
+  }
+
+  window.addEventListener("message", function(event) {
+    intent = event.data;
+    if (intent.token) {
+      location = window.location;
+      url = location.protocol + "//" + location.host + "/ds-api/request/contact/all/";
+      xhr = new XMLHttpRequest();
+      xhr.open('POST', url, true);
+      xhr.onload = function() {
+          console.log(xhr.response);
+          // $rootScope.contacts = xhr.response;
+          // $rootScope.$apply();
+      }
+      xhr.onerror = function(e) {
+          err = "Request failed : #{e.target.status}";
+          console.log(err);
+          }
+          xhr.setRequestHeader('Content-Type', 'application/json');
+          token = btoa("frontpermission:" + intent.token);
+          authorization = "Basic " + token;
+          xhr.setRequestHeader("Authorization", authorization);
+          xhr.send();
+      } else {
+          console.log("Weird intent, cannot handle it", intent);
+          window.onerror("Error handling intent: " + intent, "MainRouter.initialize", null, null,
+              new Error()
+          );
+      }
+  }, true);
+})(window);
