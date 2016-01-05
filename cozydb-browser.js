c57bfea25be543ebe5cf14ec3240b985110d3c80
lemelon <lambertfelix8@gmail.com>
2016-01-05 14:02:29 +0100

    bonjour

--- cozydb-browser.js
+++ cozydb-browser.js
@@ -1,31 +1,31 @@
-// window.parent.postMessage({action: 'getToken'}, '*');
+window.parent.postMessage({action: 'getToken'}, '*');
 
-// window.addEventListener("message", function(event) {
+window.addEventListener("message", function(event) {
 	
-// intent = event.data;
-// if (intent.token) {
-//   location = window.location;
-//   url = location.protocol + "//" + location.host + "/ds-api/request/contact/all/";
-//   xhr = new XMLHttpRequest();
-//   xhr.open('POST', url, true);
-//   xhr.onload = function() {
-//       console.log(xhr.response);
-//       $rootScope.contacts = xhr.response;
-//       $rootScope.$apply();
-//   }
-//   xhr.onerror = function(e) {
-//       err = "Request failed : #{e.target.status}";
-//       console.log(err);
-//       }
-//       xhr.setRequestHeader('Content-Type', 'application/json');
-//       token = btoa("frontpermission:" + intent.token);
-//       authorization = "Basic " + token;
-//       xhr.setRequestHeader("Authorization", authorization);
-//       xhr.send();
-//   } else {
-//       console.log("Weird intent, cannot handle it", intent);
-//       window.onerror("Error handling intent: " + intent, "MainRouter.initialize", null, null,
-//           new Error()
-//       );
-//   }
-// }, true);
+intent = event.data;
+if (intent.token) {
+  location = window.location;
+  url = location.protocol + "//" + location.host + "/ds-api/request/contact/all/";
+  xhr = new XMLHttpRequest();
+  xhr.open('POST', url, true);
+  xhr.onload = function() {
+      console.log(xhr.response);
+      $rootScope.contacts = xhr.response;
+      $rootScope.$apply();
+  }
+  xhr.onerror = function(e) {
+      err = "Request failed : #{e.target.status}";
+      console.log(err);
+      }
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      token = btoa("frontpermission:" + intent.token);
+      authorization = "Basic " + token;
+      xhr.setRequestHeader("Authorization", authorization);
+      xhr.send();
+  } else {
+      console.log("Weird intent, cannot handle it", intent);
+      window.onerror("Error handling intent: " + intent, "MainRouter.initialize", null, null,
+          new Error()
+      );
+  }
+}, true);
