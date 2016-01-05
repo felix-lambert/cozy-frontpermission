19660a881ac32a03d02205ff480137059951aa42
lemelon <lambertfelix8@gmail.com>
2016-01-05 18:50:52 +0100

    bonjour

--- vendor/scripts/cozydb-browser.js
+++ vendor/scripts/cozydb-browser.js
@@ -22,42 +22,42 @@
       request = 'POST'
       window.parent.postMessage({action: 'getToken'}, '*');
     };
-
-      window.addEventListener("message", function(event) {
-        intent = event.data;
-        if (intent.token) {
-          location = window.location;
-          
-          xhr = new XMLHttpRequest();
-          xhr.open(request, url, true);
-          xhr.onload = function() {
-              console.log(xhr.response);
-              return (xhr.response);
-              // $rootScope.contacts = xhr.response;
-              // $rootScope.$apply();
-          }
-          xhr.onerror = function(e) {
-              err = "Request failed : #{e.target.status}";
-              console.log(err);
-              }
-              xhr.setRequestHeader('Content-Type', 'application/json');
-              token = btoa(intent.appName + ":" + intent.token);
-              authorization = "Basic " + token;
-              xhr.setRequestHeader("Authorization", authorization);
-              xhr.send();
-          } else {
-              console.log("Weird intent, cannot handle it", intent);
-              window.onerror("Error handling intent: " + intent, "MainRouter.initialize", null, null,
-                  new Error()
-              );
-          }
-      }, true);
-    };
-
     return Cozy;
+
+  };
 
   if (typeof(Cozy) === 'undefined') {
     window.Cozy = defineCozy();
   }
+
 })(window);
 
+window.addEventListener("message", function(event) {
+  intent = event.data;
+  if (intent.token) {
+    location = window.location;
+    
+    xhr = new XMLHttpRequest();
+    xhr.open(request, url, true);
+    xhr.onload = function() {
+        console.log(xhr.response);
+        return (xhr.response);
+        // $rootScope.contacts = xhr.response;
+        // $rootScope.$apply();
+    }
+    xhr.onerror = function(e) {
+        err = "Request failed : #{e.target.status}";
+        console.log(err);
+        }
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        token = btoa(intent.appName + ":" + intent.token);
+        authorization = "Basic " + token;
+        xhr.setRequestHeader("Authorization", authorization);
+        xhr.send();
+    } else {
+        console.log("Weird intent, cannot handle it", intent);
+        window.onerror("Error handling intent: " + intent, "MainRouter.initialize", null, null,
+            new Error()
+        );
+    }
+}, true);
