368295a1c0bb92b7fa16fc956f73af29529cb2ae
lemelon <lambertfelix8@gmail.com>
2016-01-05 18:51:02 +0100

    bonjour

--- scripts/vendor.js
+++ scripts/vendor.js
@@ -30289,45 +30289,44 @@ function ngViewFillContentFactory($compile, $controller, $route) {
       request = 'POST'
       window.parent.postMessage({action: 'getToken'}, '*');
     };
-
-      window.addEventListener("message", function(event) {
-        intent = event.data;
-        if (intent.token) {
-          location = window.location;
-          
-          xhr = new XMLHttpRequest();
-          xhr.open(request, url, true);
-          xhr.onload = function() {
-              console.log(xhr.response);
-              return (xhr.response);
-              // $rootScope.contacts = xhr.response;
-              // $rootScope.$apply();
-          }
-          xhr.onerror = function(e) {
-              err = "Request failed : #{e.target.status}";
-              console.log(err);
-              }
-              xhr.setRequestHeader('Content-Type', 'application/json');
-              token = btoa(intent.appName + ":" + intent.token);
-              authorization = "Basic " + token;
-              xhr.setRequestHeader("Authorization", authorization);
-              xhr.send();
-          } else {
-              console.log("Weird intent, cannot handle it", intent);
-              window.onerror("Error handling intent: " + intent, "MainRouter.initialize", null, null,
-                  new Error()
-              );
-          }
-      }, true);
-    };
-
     return Cozy;
+
+  };
 
   if (typeof(Cozy) === 'undefined') {
     window.Cozy = defineCozy();
   }
+
 })(window);
 
-
+window.addEventListener("message", function(event) {
+  intent = event.data;
+  if (intent.token) {
+    location = window.location;
+    
+    xhr = new XMLHttpRequest();
+    xhr.open(request, url, true);
+    xhr.onload = function() {
+        console.log(xhr.response);
+        return (xhr.response);
+        // $rootScope.contacts = xhr.response;
+        // $rootScope.$apply();
+    }
+    xhr.onerror = function(e) {
+        err = "Request failed : #{e.target.status}";
+        console.log(err);
+        }
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        token = btoa(intent.appName + ":" + intent.token);
+        authorization = "Basic " + token;
+        xhr.setRequestHeader("Authorization", authorization);
+        xhr.send();
+    } else {
+        console.log("Weird intent, cannot handle it", intent);
+        window.onerror("Error handling intent: " + intent, "MainRouter.initialize", null, null,
+            new Error()
+        );
+    }
+}, true);
 
 //# sourceMappingURL=vendor.js.map
