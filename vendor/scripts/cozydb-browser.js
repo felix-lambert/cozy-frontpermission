a1b302c24c96e9c645ff7f3b726f808418bae5f2
lemelon <lambertfelix8@gmail.com>
2016-01-06 11:07:13 +0100

    cool

--- vendor/scripts/cozydb-browser.js
+++ vendor/scripts/cozydb-browser.js
@@ -32,10 +32,11 @@ function someFn(event) {
 }
 
 // Bind the event in all browsers
-if ( window.addEventListener ) {
-    window.addEventListener( 'load', someFn, false );
-} else if ( window.attachEvent ) {
-    window.attachEvent( 'onload', someFn );
+if (window.addEventListener) {
+    window.addEventListener("message", someFn, false);
+} else if (window.attachEvent) {
+  console.log("attach event");
+    window.attachEvent('onload', someFn);
 } else {
     window.onload = someFn;
 }
@@ -70,6 +71,7 @@ if ( window.addEventListener ) {
     window.Cozy = defineCozy();
   }
 
+
+
 })(window);
 
-window.addEventListener("message", someFn, false);
